ad4d4e48b5232d844ddc6773a2f28aa06e484cdc
Davis Kim <dchk90@gmail.com>
2014-06-20 21:19:30 -0700

    (refactor) Use star/planet classes

--- app/src/views/levels/Level1View.js
+++ app/src/views/levels/Level1View.js
@@ -18,6 +18,9 @@ define(function(require, exports, module) {
 
     var MenuView      = require('views/MenuView');
 
+    var Star = require('classes/Star');
+    var Planet = require('classes/Planet');
+
     function Level1View() {
         View.apply(this, arguments);
 
@@ -45,69 +48,10 @@ define(function(require, exports, module) {
     }
 
     function _addSun() {
-        var star = {};
-        star.surface =  new Surface({
-            size: [20, 20],
-            properties: {
-                background: '#F2B134',
-                borderRadius: '50%',
-                border: '2px solid #222',
-                zIndex: 1
-            }
-        });
-
-        star.particle = new Circle({
-            position: [
-                window.innerWidth / 2,
-                window.innerHeight / 2.5,
-                0
-            ]
-        });
-
-        // initialize true position
-        var _truePosition = star.particle.position.get();
-
-        // this is what determines where planet shows up in the UI
-        star.modifier = new Modifier({
-            transform: function() {
-                return Transform.translate(star.truePosition()[0], star.truePosition()[1], star.truePosition()[2]);
-            }
-        });
-
-        star.truePosition = function() {
-            if(star.override) {
-                star.particle.setPosition(star._truePosition);
-            } else {
-                star._truePosition = star.particle.getPosition();
-            }
-            return star._truePosition;
-        };
-
-        star.sync = new GenericSync(['mouse', 'touch']);
-        star.surface.pipe(star.sync);
-
-        // initialize override as falsy
-        star.sync.on('start', function() { star.override = true; });
-
-        // set trueposition via delta
-        star.sync.on('update', function(data) {
-            var cache = star.truePosition();
-            star._truePosition = [
-                cache[0] + data.delta[0],
-                cache[1] + data.delta[1],
-                cache[2]
-            ];
-        });
-
-        // preserve velocity on mouseup
-        star.sync.on('end', function(data) {
-            star.override = false;
-            star.particle.setVelocity([0, 0, 0]);
-        });
-
+        var star = new Star();
         this.physicsEngine.addBody(star.particle);
         this.add(star.modifier).add(star.surface);
-        this.star = star;
+        this.star = star; // so we can access it for planets
     }
 
     function _addPlanets() {
@@ -123,70 +67,7 @@ define(function(require, exports, module) {
     }
 
     function _addPlanet() {
-        var planet = {};
-        planet.surface =  new Surface({
-            size: [100, 100],
-            properties: {
-                // background: '#' + Math.floor(Math.random()*16777215).toString(16),
-                background: '#ED553B',
-                borderRadius: '50%',
-                border: '2px solid #222'
-            }
-        });
-
-        planet.particle = new Circle({
-            position: [
-                Math.random() * window.innerWidth / 2 + 200,
-                Math.random() * window.innerHeight / 2.5,
-                0
-            ],
-            velocity: [0, 0.2, 0]
-        });
-
-        // initialize true position
-        var _truePosition = planet.particle.position.get();
-
-        // this is what determines where planet shows up in the UI
-        planet.modifier = new Modifier({
-            transform: function() {
-                return Transform.translate(planet.truePosition()[0], planet.truePosition()[1], planet.truePosition()[2]);
-            }
-        });
-
-        planet.truePosition = function() {
-            if(planet.override) {
-                planet.particle.setPosition(planet._truePosition);
-            } else {
-                planet._truePosition = planet.particle.getPosition();
-            }
-            return planet._truePosition;
-        };
-
-        planet.sync = new GenericSync(['mouse', 'touch']);
-        planet.surface.pipe(planet.sync);
-
-        // initialize override as falsy
-        planet.sync.on('start', function() { planet.override = true; });
-
-        // set trueposition via delta
-        planet.sync.on('update', function(data) {
-            var cache = planet.truePosition();
-            planet._truePosition = [
-                cache[0] + data.delta[0],
-                cache[1] + data.delta[1],
-                cache[2]
-            ];
-        });
-
-        // preserve velocity on mouseup
-        planet.sync.on('end', function(data) {
-            planet.override = false;
-            planet.particle.setVelocity([
-                data.velocity[0],
-                data.velocity[1],
-                0
-            ]);
-        });
+        var planet = new Planet();
 
         this.physicsEngine.addBody(planet.particle);
         this.physicsEngine.attach(this.repulsion, planet.particle, this.star.particle);
@@ -202,7 +83,7 @@ define(function(require, exports, module) {
         menuView.on('menuItemClicked', function(data){
             console.log('item', data, 'clicked');
             if(data === 0) {
-                _addPlanet.call(this);
+                _addSun.call(this);
             } else if(data === 1) {
                 // console.log(this);
                 // this.physicsEngine.removeBody(this.physicsEngine.getBodies()[0]);
