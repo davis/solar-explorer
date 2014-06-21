ee9b949bc76186a054513842f95aa88b2202670a
Davis Kim <dchk90@gmail.com>
2014-06-21 13:29:52 -0700

    (refactor) Pull out Surface/Particle mix into its own class

---
+++ app/src/classes/SurfaceParticle.js
@@ -0,0 +1,16 @@
+/*** SurfaceParticle.js ***/
+
+define(function(require, exports, module) {
+
+    function SurfaceParticle(surface, particle) {
+        this.surface = surface;
+        this.particle = particle;
+    }
+
+    // methods would go here
+    SurfaceParticle.prototype.something = function() {
+
+    };
+
+    module.exports = SurfaceParticle;
+});
